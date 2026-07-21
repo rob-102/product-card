cd07e717b028d7fa932bb1ccbdd4933eaae8c17f
Robert Sagitov <Robertsagitov@gmail.com>
2026-07-22 00:54:41 +0500

    work on bags

--- main.js
+++ main.js
@@ -1,49 +1,49 @@
-const Card = document.querySelector('.card');
+const card = document.querySelector('.card');
 const addToBasketButton = document.querySelector('#add-to-basket');
 const backgroundColorInBasket = '#bbc5ba';
-const Cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card');
 const addToBasketAllButton = document.querySelector('#add-to-basket-all');
 
 
 //6.2 красим одну карточку из корзины
 addToBasketButton.addEventListener('click',  () => {
-  Card.style.backgroundColor = backgroundColorInBasket;
+  card.style.backgroundColor = backgroundColorInBasket;
 })
 
 //6.3 красим все карточки 
 addToBasketAllButton.addEventListener('click',  () => {
-  Cards.forEach((card) => card.style.backgroundColor = backgroundColorInBasket)
+  cards.forEach((card) => card.style.backgroundColor = backgroundColorInBasket)
 })
 
 //6.4 переход на сайт "Google.com" кнопка "перейти к оплате"
 const selectToPaymentButton = document.querySelector('#select-to-payment');
-selectToPaymentButton.addEventListener('click', OpenPaymentPage)
+selectToPaymentButton.addEventListener('click', openPaymentPage);
 
 //предупреждение перед переходом на сайт
-function OpenPaymentPage() {
+function openPaymentPage() {
   const answer = confirm('ВНИМАНИЕ! перед переходом к оплате СЛОЖИТЕ суммы выбранных товаров вручную и запомните итог')
   if (answer === true) {
     window.open('https://www.google.com')
   } else {
-     return;
+    return;
   }
 }
 
 //6.5 вывод в консоль лог, сломалось оповещение alrt перестал работать после добавления 6.6
-const OutputLogButton = document.querySelector('#output-console-log');
+const outputLogButton = document.querySelector('#output-console-log');
 
-OutputLogButton.addEventListener('click', () => OutputConsoleLog('заканчиается зд 6.5'))
-function OutputConsoleLog(message) {
-  alert('дз 6.5 выполнен без ошибок')
-  console.log(message)
+outputLogButton.addEventListener('click', () => outputConsoleLog('заканчиается зд 6.5'))
+function outputConsoleLog(message) {
+  alert('дз 6.5 выполнен без ошибок');
+  console.log(message);
 }
 
 //6.6 вывод консоль лог по наведению мыши.
 
-const OutputLogMouseOver = document.querySelector('.title');
+const outputLogMouseOverTitle = document.querySelector('.title');
 
-OutputLogMouseOver.addEventListener('mouseover', () => OutputConsoleLog(OutputLogMouseOver.textContent))
-function OutputConsoleLog(message) {
+outputLogMouseOverTitle.addEventListener('mouseover', () => outputConsoleLogTitle(outputLogMouseOverTitle.textContent))
+function outputConsoleLogTitle(message) {
   console.log(message);
 }
 
